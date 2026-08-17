9f2a35ee6f7fab8b52dbf659cdd46386ceb8d73a
Trupti-Personal <tmayee2@gmail.com>
2026-08-17 16:02:20 +0530

    award acf

--- app/awards/page.tsx
+++ app/awards/page.tsx
@@ -2,6 +2,6 @@ import Awards from "@/src/pages_migrated/about/Awards";
 import { getPageBySlug } from "@/src/lib/wordpress";
 
 export default async function AwardsPage() {
-  const wordpressData = await getPageBySlug("awards-recognition");
+  const wordpressData = await getPageBySlug("awards");
   return <Awards wordpressData={wordpressData} />;
 }
